b84c99806649c260ac4aed3ae80341e4141a158a
bala-amypo <bala@amypo.in>
2025-12-18 09:15:12 +0000

    User commit

--- src/main/java/com/example/demo/util/RuleEngineUtil.java
+++ src/main/java/com/example/demo/util/RuleEngineUtil.java
@@ -2,5 +2,8 @@ package com.example.demo.util;
 import com.example.demo.model.ClaimRule;
 import java.util.List;
 public class RuleEngineUtil{
-    public static int evaluate(String text,List)
+    public static int evaluate(String text,List<ClaimRule>rules){
+        int scores=0;
+        for(ClaimRule rule)
+    }
 }
