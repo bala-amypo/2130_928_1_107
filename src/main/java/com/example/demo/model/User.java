1aa655116657ba616f54cd16422a11ba030a9c70
bala-amypo <bala@amypo.in>
2025-12-18 09:07:30 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,1 +1,11 @@
-package com.example
+package com.example.demo.model;
+import jakarta.persistence.*;
+@Entity
+@Table(name="users",uniqueContraints=
+@UniqueContraint(columnNames="email"))
+public class User{
+    @Id @GenerateValue
+    private Long id;
+    private STring name;
+    private
+}
