2ab0e019674e1c6d4187f9744a2f856daff83ac0
bala-amypo <bala@amypo.in>
2025-12-18 09:20:35 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,4 +1,11 @@
 package com.example.demo.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework
+import org.springframework.context.annotation.*;
+@Configuration
+public class SwaggerConfig{
+    @Bean
+    public OpenAPI api(){
+        return new OpenAPI().info(new Info().title("Parcel Damage Claim API").version("1.0"));
+    }
+}
