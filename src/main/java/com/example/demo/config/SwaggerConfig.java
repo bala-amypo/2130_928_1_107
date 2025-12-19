ca54454a06efd2b6dfc265d6b865308db0429e78
bala-amypo <bala@amypo.in>
2025-12-19 05:04:56 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,11 +1,30 @@
 package com.example.demo.config;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.*;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
-public class SwaggerConfig{
+public class SwaggerConfig {
+
     @Bean
-    public OpenAPI api(){
-        return new OpenAPI().info(new Info().title("Parcel Damage Claim API").version("1.0"));
+    public OpenAPI openAPI() {
+
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Parcel Damage Claim Validator API")
+                        .version("1.0")
+                        .description("API documentation for Parcel Damage Claim Validator"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .schemaRequirement("Bearer Authentication", bearerAuth);
     }
 }
