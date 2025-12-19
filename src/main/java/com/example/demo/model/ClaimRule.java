682313bdd215730b2f5f19f982e934a60796dd82
bala-amypo <bala@amypo.in>
2025-12-19 08:31:46 +0000

    User commit

--- src/main/java/com/example/demo/model/ClaimRule.java
+++ src/main/java/com/example/demo/model/ClaimRule.java
@@ -1,20 +1,22 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
-@Data
-@NoArgsConstructor
 @Entity
-@Table(name = "claim_rules")
 public class ClaimRule {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String keyword;
-
     private int weight;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getKeyword() { return keyword; }
+    public void setKeyword(String keyword) { this.keyword = keyword; }
+
+    public int getWeight() { return weight; }
+    public void setWeight(int weight) { this.weight = weight; }
 }
