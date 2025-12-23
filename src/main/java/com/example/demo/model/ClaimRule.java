b675f71ed334506bd167b86d159a4c764e0333c1
bala-amypo <bala@amypo.in>
2025-12-23 03:55:45 +0000

    User commit

--- src/main/java/com/example/demo/model/ClaimRule.java
+++ src/main/java/com/example/demo/model/ClaimRule.java
@@ -1,13 +1,19 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "claim_rule")
 public class ClaimRule {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String keyword;
     private int weight;
 
